34cc68674616b27e3fc0f3b4cce92ee8877ae125
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-10-15 19:40:31 +0530

    Learned Context API

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <img
+        className="p-8 rounded-t-lg"
+        src="https://images.pexels.com/photos/28880498/pexels-photo-28880498/free-photo-of-scenic-beach-day-with-crowds-enjoying-the-coastline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="product_image1"
+      />
+    </div>
+  );
+}
+
+export default Card;
